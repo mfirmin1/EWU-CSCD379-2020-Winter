fcbc0c5d6dead4d0cfa0be7b21ee82a3e3daa11f
Matthew Firmin <mfirmin@eagles.ewu.edu>
2020-01-20 18:33:56 -0800

    Created class, modified Group and User, and started working on ApplicationDbContexts

---
+++ SecretSanta/src/SecretSanta.Data/Group.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Data
+{
+    public class Group : FingerPrintyEntityBase
+    {
+        private string Name;
+    }
+}
