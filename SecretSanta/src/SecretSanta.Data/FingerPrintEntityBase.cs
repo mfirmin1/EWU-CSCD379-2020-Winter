68f7112e658c2c721903abc9c46bdf956e7faa44
Matthew Firmin <mfirmin@eagles.ewu.edu>
2020-01-28 13:48:10 -0800

    Added required FingerPrintEntityBase and EntityBase

--- SecretSanta/src/SecretSanta.Data/FingerPrintEntityBase.cs
+++ SecretSanta/src/SecretSanta.Data/FingerPrintEntityBase.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 namespace SecretSanta.Data
 {
     public class FingerPrintEntityBase : EntityBase
     {
+        [Required]
         public string CreatedBy { get; set; } = String.Empty;
+        [Required]
         public DateTime CreatedOn { get; set; }
+        [Required]
         public string ModifiedBy { get; set; } = String.Empty;
+        [Required]
         public DateTime ModifiedOn { get; set; }
     }
 }
