eacc407c35f8954570f3249cc57c1af18e985749
Matthew Firmin <mfirmin@eagles.ewu.edu>
2020-02-04 00:03:43 -0800

    added the other two test files

---
+++ SecretSanta/test/SecretSanta.Api.Tests/GiftControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Moq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecretSanta.Api.Controllers;
+using SecretSanta.Business;
+using SecretSanta.Business.Services;
+using SecretSanta.Data;
+using System.Threading.Tasks;
+using SecretSanta.Data.Tests;
+
+namespace SecretSanta.Api.Tests
+{
+    public class GiftControllerTest
+    {
+    }
+}
